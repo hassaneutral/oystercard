bed1aecfdb1ddb280ba042c2e1e6b28d4e329986
Jonathan <j_passe@live.fr>
2016-05-30 16:34:43 +0100

    Chapter 04 : created test to see starting balance, made the test pass

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,14 @@
 require 'oystercard'
 
 describe Oystercard do
-	
+  subject(:oystercard) {described_class.new}
+
+  describe '#balance' do
+
+    it 'allows user to see starting balance' do
+      expect(oystercard.balance).to eq 0
+    end
+  end
+
+
 end
