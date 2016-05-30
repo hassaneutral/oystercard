3d58b5dfe78c21b3402d3f3b8f8b9987789b28bf
riyapabari <riya.pabari@gmail.com>
2016-05-30 16:55:51 +0100

    Challenge 05: Added top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,7 +8,22 @@ describe Oystercard do
     it 'allows user to see starting balance' do
       expect(oystercard.balance).to eq 0
     end
+    
   end
 
+  describe '#top_up' do
+
+  	it 'responds to method with 1 argument' do
+  		expect(oystercard).to respond_to(:top_up).with(1).argument 
+  	end
+  	it 'tops up oystercard' do
+  		expect(oystercard.top_up(1)).to eq 1
+  	end
+  	it 'tops up oystercard with non-zero balance' do
+  		oystercard.top_up(1)
+  		expect(oystercard.top_up(2)).to eq 3
+  	end
+
+  end
 
 end
