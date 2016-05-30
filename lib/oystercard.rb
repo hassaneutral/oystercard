c04466665c35f3a374168f1e4b4c3a7bbf8a2263
Jonathan <j_passe@live.fr>
2016-05-30 17:19:47 +0100

    Chapter 06 :  created a test for the maximum balance topping up value, passe the test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,11 +2,14 @@ class Oystercard
 
   attr_reader :balance
 
+  MAX_LIMIT = 90
+
   def initialize
     @balance = 0
   end
 
   def top_up(amount)
+    fail "Balance can not exceed £#{MAX_LIMIT}" if @balance + amount > MAX_LIMIT
   	@balance += amount
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -8,13 +8,13 @@ describe Oystercard do
     it 'allows user to see starting balance' do
       expect(oystercard.balance).to eq 0
     end
-    
+
   end
 
   describe '#top_up' do
 
   	it 'responds to method with 1 argument' do
-  		expect(oystercard).to respond_to(:top_up).with(1).argument 
+  		expect(oystercard).to respond_to(:top_up).with(1).argument
   	end
   	it 'tops up oystercard' do
   		expect(oystercard.top_up(1)).to eq 1
@@ -23,6 +23,11 @@ describe Oystercard do
   		oystercard.top_up(1)
   		expect(oystercard.top_up(2)).to eq 3
   	end
+    it 'raises an error if balance exceeds max limit' do
+      oystercard.top_up(Oystercard::MAX_LIMIT)
+      expect{oystercard.top_up(1)}.to raise_error "Balance can not exceed £#{Oystercard::MAX_LIMIT}"
+    end
+
 
   end
 
