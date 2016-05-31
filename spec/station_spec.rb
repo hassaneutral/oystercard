8e5231a04a6f4d10924ed20469472bc2bc852153
Hassan Radheyyan <hassanradheyyan@Hassans-MacBook-Pro.local>
2016-05-31 18:03:26 +0100

    Chapter 13: Added Name & Zone as attributes of the Station class

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,5 +1,19 @@
 describe Station do
 
-  subject(:station) {describe_class.new}
+  subject(:station) {described_class.new(:name,:zone)}
+
+  describe '#zone' do
+    it "allows to set the station's zone" do
+      station2 = Station.new("Station 2",2)
+      expect(station2.zone).to eq 2
+    end
+  end
+
+  describe '#name' do
+    it "allows to set the station's name" do
+      station2 = Station.new("Station 2",2)
+      expect(station2.name).to eq "Station 2"
+    end
+  end
 
 end
